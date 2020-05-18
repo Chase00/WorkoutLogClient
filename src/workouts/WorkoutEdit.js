fbbc30e424f9d0c23280a6df3376cb9657a4c4cb
Chase00 <chasekuhnmc@gmail.com>
2020-05-18 16:15:27 -0400

    Edit modal

--- src/workouts/WorkoutEdit.js
+++ src/workouts/WorkoutEdit.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import { Modal, ModalHeader, ModalBody, FormGroup, Label, Form, Input, Button } from 'reactstrap';
 
 const WorkoutEdit = (props) => {
 
@@ -7,9 +8,30 @@ const WorkoutEdit = (props) => {
     const [editRes, setEditRes] = useState(props.workoutToUpdate.result);
 
     return(
-        <>
-        This is Workout Edit
-        </>
+        <Modal isOpen={true}>
+            <ModalHeader>Log a Workout</ModalHeader>
+            <ModalBody>
+                <Form>
+                    <FormGroup>
+                        <Label htmlFor="result">Edit Result: </Label>
+                        <Input name="result" value={editRes} onChange={(e) => setEditRes(e.target.value)}/>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label htmlFor="description">Edit Description: </Label>
+                        <Input name="description" value={editDesc} onChange={(e) => setEditDesc(e.target.value)}/>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label htmlFor="definition">Edit Definition: </Label>
+                        <Input type="select" name="definition" value={editDef} onChange={(e) => setEditDef(e.target.value)}>
+                            <option value="Time">Time</option>
+                            <option value="Weight">Weight</option>
+                            <option value="Distance">Distance</option>
+                        </Input>
+                    </FormGroup>
+                    <Button type="submit">Update</Button>
+                </Form>
+            </ModalBody>
+        </Modal>
     )
 }
 
